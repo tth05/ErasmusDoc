adbfa8bc7be43f75689ac95c8a8dd67cf6dc5f26
KotlinIsCool <ichmagbagga@gmail.com>
2018-09-09 12:42:19 +0200

    Separate BottomAppBar to own file

--- lib/screens/app_home/app_home.dart
+++ lib/screens/app_home/app_home.dart
@@ -4,6 +4,7 @@
 */
 import 'package:erasmus_app/globals.dart' as globals;
 import 'package:erasmus_app/screens/app_home/custom_button_location.dart';
+import 'package:erasmus_app/screens/app_home/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class AppHomeScreen extends StatefulWidget {
@@ -29,42 +30,7 @@ class AppHomeState extends State<AppHomeScreen> {
         label: Text("Neuer Eintrag"),
       ),
       floatingActionButtonLocation: CustomCenterDockedFloatingActionButtonLocation(18.0),
-      bottomNavigationBar: BottomAppBar(
-        elevation: 2.0,
-        color: Theme
-            .of(context)
-            .primaryColor,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(left: 10.0),
-              child: Row(
-                children: <Widget>[
-//                  Image.asset(
-//                    "assets/images/erasmus.png",
-//                    fit: BoxFit.contain,
-//                    scale: 5.0,
-//                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 5.0),
-                    child: Text(
-                      "Erasmus-Projektinfo",
-                      style: TextStyle(color: Colors.white, fontSize: 25.0),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            IconButton(
-              color: Colors.white,
-              icon: Icon(Icons.arrow_forward_ios),
-              onPressed: () {},
-            ),
-          ],
-        ),
-      ),
+      bottomNavigationBar: ErasmusBottomAppBar(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,

f82bf7c8d72379c1a134c875bccc7e360de3157c
KotlinIsCool <ichmagbagga@gmail.com>
2019-02-25 00:18:05 +0100

    Add more data

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,7 +4,7 @@
 // find child widgets in the widget tree, read text, and verify that the values of widget properties
 // are correct.
 
-import 'package:erasmus_app/main.dart';
+import 'package:erasmus_doc/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
